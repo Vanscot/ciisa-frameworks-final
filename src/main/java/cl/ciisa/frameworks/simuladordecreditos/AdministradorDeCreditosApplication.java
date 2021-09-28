518fe99f1f1e42b08e435d7c3db4cbe3d7994fb2
Andres Gonzalez <vanscot@gmail.com>
2021-09-27 22:57:15 -0300

    Commit inicial

---
+++ src/main/java/cl/ciisa/frameworks/simuladordecreditos/AdministradorDeCreditosApplication.java
@@ -0,0 +1,13 @@
+package cl.ciisa.frameworks.simuladordecreditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorDeCreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorDeCreditosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ciisa/andregon/frameworks/simuladordecreditos/AdministradorDeCreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ciisa.andregon.frameworks.simuladordecreditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorDeCreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
